fe3b12c6d256b189a28f32c5ecd714f9c1f5cf5b
kabirbasheer <kabir.basheer@snowpact.com>
2024-11-10 16:14:46 +0100

    Initial commit

---
+++ .history/frontend/frontend/tailwind.config_20241110154304.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Ajoute ici les extensions de fichiers que tu utilises
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
